8bea036739b331ae89d4904dc37b9b37d1c8dc6b
Alexandr Zhdanov <git.user.AlAlAZHdanov@inno.tech>
2025-11-13 14:02:15 +0300

    добавлен ввод адреса файла с условиями для проверки корректности

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,32 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>:");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+
+
+        //задание по теме "Циклы"
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if(!fileExists){
+                System.out.println("Файла не существует.");
+                count++;
+                continue;
+            }
+            if(isDirectory){
+                System.out.println("Это папка.");
+                count++;
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно. Это файл номер " + count);
+        }
+
+
+
+
     }
 }

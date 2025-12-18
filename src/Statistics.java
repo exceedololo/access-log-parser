9bb5787c0b50cf26d3c962f1053d2a9149005eba
Alexandr Zhdanov <git.user.AlAlAZHdanov@inno.tech>
2025-12-18 13:57:37 +0300

    Задание #1 по теме "Collections"
    добавлена HashSet-переменная webSites и ее наполнение. А так же HashMap<String,Integer>-переменная osFrequencies и ее наполнение. И метод getOsFraction для подсчета доли каждой ОС в логе.

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,12 +1,16 @@
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Locale;
 
 public class Statistics {
     int totalTraffic;
     LocalDateTime minTime;
     LocalDateTime maxTime;
+    HashSet<String> webSites = new HashSet<>();
+    HashMap<String,Integer> osFrequencies = new HashMap<>();
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
 
@@ -17,12 +21,40 @@ public class Statistics {
 
     }
 
+    public int getTotalOSCount(){
+        int totalOSCount = 0;
+        for (int count : osFrequencies.values()){
+            totalOSCount += count;
+        }
+        return totalOSCount;
+    }
+
+    public HashMap<String, Double> getOsFraction(){
+        HashMap<String, Double> osFraction = new HashMap<>();
+        int totalOSCount = getTotalOSCount();
+        if (totalOSCount == 0){
+            return osFraction;
+        }
+        for (var entry : osFrequencies.entrySet()){
+            String OS = entry.getKey();
+            int count = entry.getValue();
+            double fraction = (double)count / totalOSCount;
+            osFraction.put(OS, fraction);
+        }
+        return osFraction;
+    }
+
     public LocalDateTime getMaxTime() {
         return maxTime;
     }
 
     public void addEntry(LogEntry entry){
         totalTraffic += entry.getResponseSize();
+
+        if (entry.getResponse() == 200){
+            webSites.add(entry.getUrl());
+        }
+
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(entry.getDateTime(), formatter);
         LocalDateTime time = offsetDateTime.toLocalDateTime();
         if(minTime == null || time.isBefore(minTime)){
@@ -31,6 +63,14 @@ public class Statistics {
         if(maxTime == null || time.isAfter(maxTime)){
             maxTime = time;
         }
+
+        String OS = entry.getUserAgent().getOS();
+        if(!osFrequencies.containsKey(OS)){
+            osFrequencies.put(OS, 1);
+        }else {
+            osFrequencies.put(OS,osFrequencies.get(OS) + 1);
+        }
+
     }
 
     public double  getTrafficRate(){
